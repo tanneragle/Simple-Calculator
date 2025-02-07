f2131c7d12d9b8af4d6cf7f97b5481baec2054e3
tanneragle <tanneragle@gmail.com>
2025-02-07 13:48:13 -0500

    Create calculator class containing outline of necessary functions

--- script.js
+++ script.js
@@ -5,3 +5,40 @@ const equalsButton = document.querySelector(`[data-equals]`)
 const allClearButton = document.querySelector(`[data-allClear]`)
 const currentOperandTextElement = document.querySelector(`[data-currentOperand]`)
 const previous OperandTextElement = document.querySelector(`[data-previousOperand]`)
+
+// Calculator Class
+
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement
+    }
+
+clear() {
+    this.currentOperandTextElement = ''
+    this.previousOperandTextElement = ''
+    this.operator = undefined
+}
+
+delete() {
+
+}
+
+appendNumber(number) {
+
+}
+
+chooseOperation(operator) {
+    
+}
+
+compute() {
+
+}
+
+updateDisplay() {
+
+}
+
+
+}
