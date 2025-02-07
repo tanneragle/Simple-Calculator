0cbe59d3551f47bff3190808a28a566b6637f7f0
tanneragle <tanneragle@gmail.com>
2025-02-07 13:40:32 -0500

    Add global variables for accessing HTML divs in JS

---
+++ script.js
@@ -0,0 +1,7 @@
+// Global Variables
+const numberButtons = document.querySelectorAll(`[data-number]`)
+const operatorButtons = document.querySelectorAll(`[data-operator]`)
+const equalsButton = document.querySelector(`[data-equals]`)
+const allClearButton = document.querySelector(`[data-allClear]`)
+const currentOperandTextElement = document.querySelector(`[data-currentOperand]`)
+const previous OperandTextElement = document.querySelector(`[data-previousOperand]`)
